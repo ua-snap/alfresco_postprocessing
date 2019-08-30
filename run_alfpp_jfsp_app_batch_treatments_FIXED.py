3e5bfa3d766bd3d8e9ba433d638295378fc989d2
EarthScientist <lindgren.mike@gmail.com>
2019-08-30 14:41:41 -0800

    dump data out to the Tech_Projects folder instead of atlas_scratch for clarity.

--- run_alfpp_jfsp_app_batch_treatments_FIXED.py
+++ run_alfpp_jfsp_app_batch_treatments_FIXED.py
@@ -15,8 +15,9 @@ name_field = 'NAME'
 metrics = [ 'veg_counts','avg_fire_size','number_of_fires','all_fire_sizes','total_area_burned','severity_counts' ]
 base_path = '/big_scratch/shiny/Runs_Statewide/paul.duffy@neptuneinc.org'
 base_path2 = '/atlas_scratch/apbennett/JFSP'
-out_path = '/atlas_scratch/malindgren/ALFRESCO_PostProcessing/JFSP_tx2/AlaskaFireManagementOptions_treatments_fixed' # this is the base out dir
+out_path = '/workspace/Shared/Tech_Projects/ALF_JFSP/project_data/ALFRESCO_PostProcessing/FireManagementOptions_fixed' # this is the base out dir
 treatment_groups = ['cru_tx0','gcm_tx0','gcm_tx1','gcm_tx2']
+# treatment_groups = ['gcm_tx2']
 shape_lu = {'tx0':os.path.join(subdomains_path,'SensitivityTX0.shp'),'tx1':os.path.join(subdomains_path,'SensitivityTX1.shp'),'tx2':os.path.join(subdomains_path,'SensitivityTX2.shp')}
 
 for group in treatment_groups:
@@ -25,14 +26,20 @@ for group in treatment_groups:
 	print('running treatment group: {}'.format(group))
 
 	if treatment == 'tx2':
-		alf_runs = os.listdir(os.path.join(base_path2, group))
+		alf_runs = os.listdir(os.path.join(base_path2, treatment))
 	else:
 		alf_runs = os.listdir(os.path.join(base_path, group))
-
 	for run_name in alf_runs:
 		print('	postprocessing alfresco group: {}'.format(run_name))
 		treatment_name,scenario,model = run_name.split('_')
-		maps_path = os.path.join(base_path, group, run_name, 'Maps')
+		if treatment == 'tx2':
+			maps_path = os.path.join(base_path2, treatment, run_name, 'Maps')
+		else:
+			maps_path = os.path.join(base_path, group, run_name, 'Maps')
+
+		# print out the treatment and maps_paths to be sure it is right:
+		print('treatment:{}\nmaps_path:{}\nshapefile:{}\n\n '.format(treatment, maps_path, subdomains_fn))
+		
 		output_path = os.path.join(out_path, group, run_name)
 		suffix = run_name # some id for the output csvs
 
